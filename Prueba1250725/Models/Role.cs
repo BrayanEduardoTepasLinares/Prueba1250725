94510028e629e8e117c9fc5087ee85f950204813
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-07-26 08:47:23 -0600

    se cambio a español los nombres con display

--- Prueba1250725/Models/Role.cs
+++ Prueba1250725/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Prueba1250725.Models;
 
@@ -7,6 +8,7 @@ public partial class Role
 {
     public int Id { get; set; }
 
+    [Display(Name = "nombre")]
     public string Name { get; set; } = null!;
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
--- Prueba1250725/Models/User.cs
+++ Prueba1250725/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Prueba1250725.Models;
 
@@ -7,13 +8,16 @@ public partial class User
 {
     public int Id { get; set; }
 
+    [Display(Name = "nombre")]
     public string Name { get; set; } = null!;
 
+    [Display(Name = "correo")]
     public string Email { get; set; } = null!;
 
+    [Display(Name = "contraseña")]
     public string Password { get; set; } = null!;
-
+    [Display(Name = "rol")]
     public int RoleId { get; set; }
-
+    [Display(Name = "rol")]
     public virtual Role? Role { get; set; } = null!;
 }
